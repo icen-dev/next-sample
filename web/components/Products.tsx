add372136da2bbd87d9b5655f1a3d407990f28f0
icen-dev <koike@icen.jp>
2022-03-18 02:55:53 +0900

    ProductDialogの追加

--- web/components/Products.tsx
+++ web/components/Products.tsx
@@ -1,40 +1,53 @@
-import { useState } from "react"
-import { Card, Container, Typography, Box, CardActionArea, CardMedia, CardContent, Chip, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button } from "@mui/material";
-import Image from "next/image"
-import Headline from "./Headline"
-import { Widgets } from "@mui/icons-material"
-
+import { useState } from "react";
+import {
+  Card,
+  Container,
+  Typography,
+  Box,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Chip,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+  ButtonBase,
+} from "@mui/material";
+import Image from "next/image";
+import Headline from "./Headline";
+import { Widgets } from "@mui/icons-material";
 
 interface DialogProps {
-  isDialogOpened: boolean
-  productData: any
+  open: boolean;
+  handleClose: any;
+  data: DataProps;
+}
+
+interface DataProps {
+  src: string;
+  title: string;
+  price: number;
+  parts: number;
 }
 
 interface ItemProps {
-  src: string
-  title: string
-  price: number
-  parts: number
+  idx: number;
+  data: DataProps;
+  handleOpen: Function;
 }
 
-const ProductDialog = () => {
-  const [open, setOpen] = useState(false)
-  const handleOpen = () => {
-    setOpen(true)
-  }
-  const handleClose = () => {
-    setOpen(false)
-  }
+const ProductDialog = (props: DialogProps) => {
   return (
     <Dialog
-      open={open}
-      onClose={handleClose}
+      open={props.open}
+      onClose={props.handleClose}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <DialogTitle id="alert-dialog-title">
-        {"Use Google's location service?"}
-      </DialogTitle>
+      <DialogTitle id="alert-dialog-title">{props.data.title}</DialogTitle>
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
           Let Google help apps determine location. This means sending anonymous
@@ -42,64 +55,109 @@ const ProductDialog = () => {
         </DialogContentText>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose}>Disagree</Button>
-        <Button onClick={handleClose} autoFocus>
+        <Button onClick={props.handleClose}>Disagree</Button>
+        <Button onClick={props.handleClose} autoFocus>
           Agree
         </Button>
       </DialogActions>
     </Dialog>
-  )
-}
+  );
+};
 
 const Item = (props: ItemProps) => {
   return (
-    <Card sx={{ m: 2 }}>
+    <Card
+      sx={{ m: 2 }}
+      onClick={() => {
+        props.handleOpen(props.idx);
+      }}
+    >
       <CardActionArea>
-        <CardMedia component="img" height="180" image={props.src} alt="test"></CardMedia>
+        <CardMedia
+          component="img"
+          height="180"
+          image={props.data.src}
+          alt="test"
+        ></CardMedia>
 
         <CardContent>
-          <Typography component="div" sx={{ fontWeight: "bold" }}>{props.title}</Typography>
-          <Typography sx={{ fontWeight: "bold" }}>¥{props.price} -</Typography>
+          <Typography component="div" sx={{ fontWeight: "bold" }}>
+            {props.data.title}
+          </Typography>
+          <Typography sx={{ fontWeight: "bold" }}>
+            ¥{props.data.price} -
+          </Typography>
         </CardContent>
 
-        <Chip icon={<Widgets />} sx={{ position: "absolute", px: 1, top: 10, right: 10 }} size="small" label={`${props.parts} Parts`} color="primary" variant="outlined" />
+        <Chip
+          icon={<Widgets />}
+          sx={{ position: "absolute", px: 1, top: 10, right: 10 }}
+          size="small"
+          label={`${props.data.parts} Parts`}
+          color="primary"
+          variant="outlined"
+        />
       </CardActionArea>
     </Card>
-  )
-}
+  );
+};
 
 const Product = () => {
-  const title = "Productions"
-  const subtitle = " Choose a Customizable Shoe Style. (10+)"
+  const title = "Productions";
+  const subtitle = " Choose a Customizable Shoe Style. (10+)";
   const itemList = [
-    { src: '/product-sample.jpg', title: 'Hole Cut', price: 42000, parts: 2 },
-    { src: '/product-sample.jpg', title: 'Straight Tip', price: 58000, parts: 3 },
-    { src: '/product-sample.jpg', title: 'Loafer', price: 23000, parts: 2 },
-    { src: '/product-sample.jpg', title: 'Double Mounk', price: 59000, parts: 5 },
-    { src: '/product-sample.jpg', title: 'Hole Cut', price: 42000, parts: 3 },
-    { src: '/product-sample.jpg', title: 'Straight Tip', price: 58000, parts: 3 },
-    { src: '/product-sample.jpg', title: 'Loafer', price: 23000, parts: 4 },
-    { src: '/product-sample.jpg', title: 'Double Mounk', price: 59000, parts: 2 },
-  ]
-  const isDialogOpened = false
+    { src: "/product-sample.jpg", title: "Hole Cut", price: 42000, parts: 2 },
+    {
+      src: "/product-sample.jpg",
+      title: "Straight Tip",
+      price: 58000,
+      parts: 3,
+    },
+    { src: "/product-sample.jpg", title: "Loafer", price: 23000, parts: 2 },
+    {
+      src: "/product-sample.jpg",
+      title: "Double Mounk",
+      price: 59000,
+      parts: 5,
+    },
+    { src: "/product-sample.jpg", title: "Hole Cut", price: 42000, parts: 3 },
+    {
+      src: "/product-sample.jpg",
+      title: "Straight Tip",
+      price: 58000,
+      parts: 3,
+    },
+    { src: "/product-sample.jpg", title: "Loafer", price: 23000, parts: 4 },
+    {
+      src: "/product-sample.jpg",
+      title: "Double Mounk",
+      price: 59000,
+      parts: 2,
+    },
+  ];
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState(0);
+  const handleOpen = (idx: number) => {
+    setSelected(idx);
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Container sx={{ p: 3 }}>
-      <ProductDialog />
+      <ProductDialog open={open} handleClose={handleClose} data={itemList[selected]} />
       <Headline title={title} subtitle={subtitle} />
-      <Box>
-        {itemList.map((v) => {
-          return (
-            <Item
-              src={v.src}
-              title={v.title}
-              price={v.price}
-              parts={v.parts} />
-          )
+      <Box
+        sx={{ display: "grid", gap: 1, gridTemplateColumns: { xs: "repeat(2, 1fr)", md: "repeat(4, 1fr)" } }}
+      >
+        {itemList.map((v, i) => {
+          return <Item idx={i} data={v} handleOpen={handleOpen} />;
         })}
       </Box>
     </Container>
   );
 };
 
-export default Product
+export default Product;
--- web/components/Theme.tsx
+++ web/components/Theme.tsx
@@ -1,6 +1,28 @@
 import { createTheme } from "@mui/material/styles";
 
+// declare module '@mui/material/styles' {
+//   interface BreakpointOverrides {
+//     xs: false; // removes the `xs` breakpoint
+//     sm: false;
+//     md: false;
+//     lg: false;
+//     xl: false;
+//     mobile: true; // adds the `mobile` breakpoint
+//     tablet: true;
+//     laptop: true;
+//     desktop: true;
+//   }
+// }
+
 export const theme = createTheme({
+  // breakpoints: {
+  //   values: {
+  //     mobile: 0,
+  //     tablet: 640,
+  //     laptop: 1024,
+  //     desktop: 1200,
+  //   },
+  // },
   typography: {
     fontFamily: [
       "Noto Serif JP",
